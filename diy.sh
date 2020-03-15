567cab19ee1da562e393eecfd6f6047bcb2aa8f3
yingdk <59006685+yingdk@users.noreply.github.com>
2020-03-15 19:36:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
 ./scripts/feeds update -a
 ./scripts/feeds install -a
