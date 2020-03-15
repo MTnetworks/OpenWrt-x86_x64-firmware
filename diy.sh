abde2ffbacb1217b9a889eb55d0db08ef653989e
yingdk <59006685+yingdk@users.noreply.github.com>
2020-03-15 23:44:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
-cd lede
+cd openwrt
 ./scripts/feeds update -a 
 ./scripts/feeds install -a
