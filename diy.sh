5c34468761f8a8046dd39540b38bdc3b04993d03
yingdk <59006685+yingdk@users.noreply.github.com>
2020-03-15 23:40:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,5 +8,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-run: git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
-run: cd openwrt && ./scripts/feeds update -a && ./scripts/feeds install -a
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
+cd lede
+./scripts/feeds update -a 
+./scripts/feeds install -a
