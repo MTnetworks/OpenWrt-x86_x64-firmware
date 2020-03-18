2e4523920058d962d80887bf0dd65d1c1e0f09eb
yingdk <59006685+yingdk@users.noreply.github.com>
2020-03-18 15:46:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,17 +12,13 @@
 #rm -rf package/lean/luci-theme-argon
 #添加新的主题
 #git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
 #取消bootstrap为默认主题
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除默认密码
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
-
 #-更新feeds
-#cd openwrt && ./scripts/feeds update -a
-
-#安装feeds
-#cd openwrt && ./scripts/feeds install -a
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomato package/lean/luci-theme-opentomato
 ./scripts/feeds update -a
+#-安装feeds
 ./scripts/feeds install -a
